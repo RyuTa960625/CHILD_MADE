1a4e3720910397e041a5a468088623dc9ec2a552
kdozlo <eodud5730@naver.com>
2024-01-25 01:24:11 +0900

    [BE] Init : 프로젝트 생성

---
+++ backend/src/main/java/com/d209/childmade/ChildMadeApplication.java
@@ -0,0 +1,13 @@
+package com.d209.childmade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChildMadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChildMadeApplication.class, args);
+	}
+
+}
