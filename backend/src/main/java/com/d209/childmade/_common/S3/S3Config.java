b1eb082fe4109fd9a171bf6ea7b828a63bfee979
박성진 <p7364009@gmail.com>
2024-01-29 12:57:11 +0900

    [BE] Feat : S3 연결 설정 파일 생성

---
+++ backend/src/main/java/com/d209/childmade/_common/S3/S3Config.java
@@ -0,0 +1,32 @@
+package com.d209.childmade._common.S3;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class S3Config {
+
+    @Value("${cloud.aws.credentials.accessKey}")
+    private String accessKey;   // S3 AccessKey
+    @Value("${cloud.aws.credentials.secretKey}")
+    private String secretKey;   // S3 SecretKey
+    @Value("${cloud.aws.region.static}")
+    private String region;  // S3 Region
+
+    @Bean
+    public AmazonS3Client amazonS3Client(){
+
+        AWSCredentials awsCredentials = new BasicAWSCredentials(accessKey, secretKey);
+        return (AmazonS3Client) AmazonS3ClientBuilder.standard()
+                .withRegion(region)
+                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+                .build();
+    }
+
+}
