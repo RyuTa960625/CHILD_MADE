14cc4a91ab67d0fa7a0c13aa425f6606cd7a0218
wnstj7866 <wnstj7866@naver.com>
2024-01-30 14:58:43 +0900

    [BE] Fix : SuccessResponse 수정

--- backend/src/main/java/com/d209/childmade/_common/response/SuccessResponse.java
+++ backend/src/main/java/com/d209/childmade/_common/response/SuccessResponse.java
@@ -8,15 +8,22 @@ import lombok.Getter;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class SuccessResponse <T> {
 
-    private int statusCode; //http status code
-    private String msg; //response message
-    private T data; //response data
+    private final int statusCode; //http status code
+    private final String msg; //response message
+    private final T data; //response data
 
     @Builder
     private SuccessResponse(int statusCode, String msg, T data) {
         this.statusCode = statusCode;
         this.msg = msg;
         this.data = data;
+    }
+
+    public static SuccessResponse<Void> from(SuccessType successType) {
+        return SuccessResponse.<Void>builder()
+                .statusCode(successType.getStatus().value())
+                .msg(successType.getMsg())
+                .build();
     }
 
     public static <T> SuccessResponse<T> of(SuccessType successType, T data) {
