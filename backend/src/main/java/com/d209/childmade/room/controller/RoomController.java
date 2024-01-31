b9ca6392a3d217f8b11aae58bbeb3137e153c57e
kdozlo <eodud5730@naver.com>
2024-02-01 04:39:28 +0900

    [BE] Style : 코트 포맷팅

--- backend/src/main/java/com/d209/childmade/room/controller/RoomController.java
+++ backend/src/main/java/com/d209/childmade/room/controller/RoomController.java
@@ -9,15 +9,7 @@ import com.d209.childmade.room.service.RoomService;
 import io.openvidu.java.client.OpenViduHttpException;
 import io.openvidu.java.client.OpenViduJavaClientException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin(origins = "*")
 @RestController
