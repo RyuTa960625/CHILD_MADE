1e427731e736c808d33ab9d334ce1fb638375afa
wnstj7866 <wnstj7866@naver.com>
2024-02-01 17:54:45 +0900

    [BE] Feat : MemberRoomRepository 메서드 추가

--- backend/src/main/java/com/d209/childmade/room/repository/MemberRoomRepository.java
+++ backend/src/main/java/com/d209/childmade/room/repository/MemberRoomRepository.java
@@ -1,9 +1,15 @@
 package com.d209.childmade.room.repository;
 
 import com.d209.childmade.room.entity.MemberRoom;
+import java.util.List;
 import java.util.Optional;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRoomRepository extends JpaRepository<MemberRoom, Long> {
+
+    Optional<MemberRoom> findByMemberId(int memberId);
+    List<MemberRoom> findAllByRoomIdAndMemberRoomIdNot(long roomId, long memberRoomId, Pageable pageable);
+
 }
 
