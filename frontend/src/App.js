ae2487157534745533ea9ec64244dab7c117e285
DESKTOP-LL7FDJ6\SSAFY <rlarbfl0702@naver.com>
2024-02-02 11:30:17 +0900

    [FE] Design : 메인화면과 상단바 구성 및 커서변경

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
+import Home from './component/Home';
+import Header from './component/Header';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <div className="App custom-cursor" >
+      <BrowserRouter>      
+        <Header/>
+        <Routes>
+          <Route path='/main' element={<Home/>}>
+            </Route>
+        </Routes>
+      </BrowserRouter>
+    </div> 
   );
 }
 
