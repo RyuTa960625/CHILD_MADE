857966e38a572ddc51a34941ce64519de94dbcc4
wnstj7866 <wnstj7866@naver.com>
2024-02-02 16:26:15 +0900

    [BE] Feat : ResponseDto 추가

---
+++ backend/src/main/java/com/d209/childmade/book/dto/response/RoleListDto.java
@@ -0,0 +1,18 @@
+package com.d209.childmade.book.dto.response;
+
+import com.d209.childmade.role.entity.Role;
+import lombok.Getter;
+
+@Getter
+public class RoleListDto {
+
+    private int roleId;
+    private String roleName;
+    private String imageUrl;
+
+    public RoleListDto(Role role) {
+        this.roleId = role.getId();
+        this.roleName = role.getRoleName();
+        this.imageUrl = role.getImageUrl();
+    }
+}
