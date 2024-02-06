2124d14369f032b3e209dfc0086e40961425f5e6
wnstj7866 <wnstj7866@naver.com>
2024-02-06 15:47:14 +0900

    [BE] Feat : ScriptService 메서드 추가

--- backend/src/main/java/com/d209/childmade/book/service/ScriptService.java
+++ backend/src/main/java/com/d209/childmade/book/service/ScriptService.java
@@ -1,5 +1,11 @@
 package com.d209.childmade.book.service;
 
+import com.d209.childmade._common.exception.CustomBadRequestException;
+import com.d209.childmade._common.response.ErrorType;
+import com.d209.childmade.book.dto.request.ScriptListRequestDto;
+import com.d209.childmade.book.entity.Script;
+import com.d209.childmade.book.repository.ScriptRepository;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -8,4 +14,15 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class ScriptService {
+
+    private final ScriptRepository scriptRepository;
+
+    public List<Script> findScriptList(int bookId, ScriptListRequestDto scriptListRequestDto) {
+        List<Script> scriptList = scriptRepository.findAllByBookIdAndBranchNum(bookId,
+                scriptListRequestDto.getBranchNum());
+        if(scriptList.isEmpty()){
+            throw new CustomBadRequestException(ErrorType.NOT_FOUND_SCRIPT_LIST);
+        }
+        return scriptList;
+    }
 }
