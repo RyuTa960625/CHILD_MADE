7e925342e90f821e986a66a6ec24811927ff61e5
wnstj7866 <wnstj7866@naver.com>
2024-02-06 15:48:57 +0900

    [BE] Refactor : RoleRepository 메서드 삭제, RoleListResponseDto 데이터 추가

--- backend/src/main/java/com/d209/childmade/book/dto/response/RoleListResponseDto.java
+++ backend/src/main/java/com/d209/childmade/book/dto/response/RoleListResponseDto.java
@@ -8,18 +8,25 @@ import lombok.Getter;
 @Getter
 public class RoleListResponseDto {
 
+    private final String title;
     private final String summary;
+    private final String bookImageUrl;
+
     private List<RoleListDto> roleListDtoList = new ArrayList<>();
 
     @Builder
-    private RoleListResponseDto(String summary, List<RoleListDto> roleListDtoList){
+    private RoleListResponseDto(String title, String summary, String bookImageUrl, List<RoleListDto> roleListDtoList){
+        this.title = title;
         this.summary = summary;
+        this.bookImageUrl = bookImageUrl;
         this.roleListDtoList = roleListDtoList;
     }
 
-    public static RoleListResponseDto of(String summary,List<RoleListDto> roleListDtoList){
+    public static RoleListResponseDto of(String title, String summary, String bookImageUrl, List<RoleListDto> roleListDtoList){
         return builder()
+                .title(title)
                 .summary(summary)
+                .bookImageUrl(bookImageUrl)
                 .roleListDtoList(roleListDtoList)
                 .build();
     }
