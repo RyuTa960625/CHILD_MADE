3ae9bfcf930fec42101e5a99076771512089ef07
DESKTOP-LL7FDJ6\SSAFY <rlarbfl0702@naver.com>
2024-02-07 14:49:57 +0900

    [FE] Design : 녹화된 동화 리스트 화면 구현과 영상 상세정보 화면 구현

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import Home from './component/Home';
 import Header from './component/Header';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import BookList from './component/BookList';
+import RecordList from './component/RecordList';
 
 function App() {
   return (
@@ -13,6 +14,8 @@ function App() {
           <Route path='/main' element={<Home/>}>
             </Route>
           <Route path='/booklist' element={<BookList/>}>
+          </Route>
+          <Route path='/recordlist' element={<RecordList/>}>
           </Route>
         </Routes>
       </BrowserRouter>
