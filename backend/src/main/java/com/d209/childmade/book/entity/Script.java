6be270b123afabced03fc33187edc8bde7834e38
박성진 <p7364009@gmail.com>
2024-02-12 20:26:05 +0900

    [BE] Fix : Script id 컬럼명 수정 및 backgroundUrl 컬럼 추가

--- backend/src/main/java/com/d209/childmade/book/entity/Script.java
+++ backend/src/main/java/com/d209/childmade/book/entity/Script.java
@@ -14,7 +14,7 @@ public class Script {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "member_room_id")
+    @Column(name = "script_id")
     private Long id;
 
     @Column(nullable = false)
@@ -23,14 +23,17 @@ public class Script {
     @Column(nullable = false)
     private String scriptLine;
 
-    @Column(nullable = false)
+    @Column(nullable = true)
+    private String backgroundUrl;
+
+    @Column(nullable = true)
     private String videoUrl;
 
     @Column(nullable = false)
     private int branchNum;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "role_id", nullable = false)
+    @JoinColumn(name = "role_id", nullable = true)
     private Role role;
 
     @ManyToOne(fetch = FetchType.LAZY)
@@ -38,19 +41,21 @@ public class Script {
     private Book book;
 
     @Builder
-    private Script(int scriptNum, String scriptLine, String videoUrl, int branchNum, Role role, Book book) {
+    private Script(int scriptNum, String scriptLine, String backgroundUrl, String videoUrl, int branchNum, Role role, Book book) {
         this.scriptNum = scriptNum;
         this.scriptLine = scriptLine;
+        this.backgroundUrl = backgroundUrl;
         this.videoUrl = videoUrl;
         this.branchNum = branchNum;
         this.role = role;
         this.book = book;
     }
 
-    public static Script of(int scriptNum, String scriptLine, String videoUrl, int branchNum, Role role, Book book) {
+    public static Script of(int scriptNum, String scriptLine, String backgroundUrl, String videoUrl, int branchNum, Role role, Book book) {
         return builder()
                 .scriptNum(scriptNum)
                 .scriptLine(scriptLine)
+                .backgroundUrl(backgroundUrl)
                 .videoUrl(videoUrl)
                 .branchNum(branchNum)
                 .role(role)
