356b2f02ab5c3d7d0de328a0c4953ec83128f2aa
이준서 <wnstj7866@naver.com>
2024-02-14 20:56:10 +0900

    [BE] Feat : helper 조회 기능 추가

--- backend/src/main/java/com/d209/childmade/role/controller/RoleController.java
+++ backend/src/main/java/com/d209/childmade/role/controller/RoleController.java
@@ -1,11 +1,26 @@
 package com.d209.childmade.role.controller;
 
+import com.d209.childmade._common.response.SuccessResponse;
+import com.d209.childmade._common.response.SuccessType;
+import com.d209.childmade.role.dto.response.HelperListResponseDto;
+import com.d209.childmade.role.service.RoleService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/roles")
 @RequiredArgsConstructor
 public class RoleController {
+
+    private final RoleService roleService;
+
+    @GetMapping("{role-id}/helpers")
+    public SuccessResponse<List<HelperListResponseDto>> getHelper(@PathVariable("role-id") int roleId){
+        return SuccessResponse.of(SuccessType.ROLE_LIST_SUCCESSFULLY,roleService.findHelperListByRole(roleId));
+    }
 }
