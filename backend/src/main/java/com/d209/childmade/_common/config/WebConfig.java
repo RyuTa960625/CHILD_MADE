18bdeecc8531807e63759eae8006f75bb3de65a0
kdozlo <eodud5730@naver.com>
2024-02-15 00:15:03 +0900

    [BE] Fix : cors 에러 해결

--- backend/src/main/java/com/d209/childmade/_common/config/WebConfig.java
+++ backend/src/main/java/com/d209/childmade/_common/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:3000", "https://i10d209.p.ssafy.io")
                 .allowedMethods("GET", "POST","PUT","DELETE")
                 .allowCredentials(true)
                 .maxAge(3000);
