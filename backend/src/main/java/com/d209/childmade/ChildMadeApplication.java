15c6125f56673040fa63d04acb5eab123308137e
kdozlo <eodud5730@naver.com>
2024-02-15 16:23:04 +0900

    [BE] Fix : 시큐리티 관련 수정

--- backend/src/main/java/com/d209/childmade/ChildMadeApplication.java
+++ backend/src/main/java/com/d209/childmade/ChildMadeApplication.java
@@ -2,10 +2,11 @@ package com.d209.childmade;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class ChildMadeApplication {
 
 	public static void main(String[] args) {
